cac1e07dfe28511321a871336155a1555b49180f
angelique KRAUSE <krause_angelique@hotmail.com>
2022-08-01 13:36:41 +0200

    try to find a solution to add a div

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,24 @@ const taslList = document.querySelector("inline");
 let boxElem = document.querySelector("#add");
 let task = document.querySelector('#task');
 
-
+// click hover the element
 boxElem.addEventListener("click", function () {
     let div = document.createElement('div');
     div.classList.add('inline');
 
-    div.innerHTML = `
+    //  div.innerHTML =
+    let z = document.createElement('div'); // is a node
+    z.innerHTML += `
     <p class="done">${task.value}</p>
         <div class="input">
             <input type="button" value="Done" id="done" />
             <input type="button" value="Delete" id="delete" />
         </div>
-    `
-    div.appendChild(body)
+    `;
+    document.body.appendChild(z);
+
+
+
+
 
 });
