36c23837c77d4dc99a47a7515732f2a3015580d8
angelique KRAUSE <krause_angelique@hotmail.com>
2022-08-04 15:51:04 +0200

    css release

--- scripts/script.js
+++ scripts/script.js
@@ -6,39 +6,38 @@ let task = document.querySelector('#task');
 // click hover the element
 boxElem.addEventListener("click", function () {
     if (task.value !== "") {
-        //  div.innerHTML =
-        let z = document.createElement('div'); // is a node
-        z.classList.add('inLine');
-        z.innerHTML = `
-    <p class="todone">${task.value}</p>
-        <div class="buttons">
-            <button class="done">Done</button>
-            <button class="delete">Delete</button>
-        </div>
-    `;
-        row.appendChild(z); // add the content of the inLineRow div
+
+        let newElement = document.createElement('div'); // is a node
+        newElement.classList.add('inLine');
+        newElement.innerHTML = `
+            <p class="todone">${task.value}</p>
+            <div class="buttons">
+                <button class="done">Done</button>
+                <button class="delete">Delete</button>
+            </div>
+        `;
+        row.appendChild(newElement); // add the content of the inLineRow div
         task.value = ""; // to clear the input task 
         let buttons = document.querySelectorAll(".inLine .buttons button");
         console.log(buttons);
 
         buttons.forEach(btn => {
             btn.addEventListener('click', function () {
-                if (btn.classList.contains('done')) {
+                if (btn.classList[0] === 'done') {
                     btn.classList.toggle('undo'); // done <=> undo
                     console.log(btn);
                     // new value of Done button
-                    btn.innerHTML === "Done" ? btn.innerHTML = "Undo" : btn.innerHTML = "Done";
+                    if (btn.innerHTML === "Done") {
+                        btn.innerHTML = "Undo";
+                    } else {
+                        btn.innerHTML = "Done";
+                    }
                 };
-
-
                 if (btn.classList.contains('delete')) {
                     btn.parentElement.parentElement.remove() // the granddad from the element and remove it
-
                 }
-
             });
         });
-
     }
     else {
         alert('input the value')
