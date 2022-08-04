77fed54beb44a40f428114df4a4b2d41386c1bc4
angelique KRAUSE <krause_angelique@hotmail.com>
2022-08-04 16:02:42 +0200

    remove

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,56 @@
 // write into HTML document
 const row = document.querySelector(".inLineRow");
-let boxElem = document.querySelector("#add");
-let task = document.querySelector('#task');
+let btnAddTask = document.getElementById('add');
+let task = document.getElementById('task');
+
+/*                 <div class="inLine">
+                    <p class="todone">First task</p>
+                    <div class="input">
+                        <input type="button" value="Done" class="done">
+                        <input type="button" value="Delete" class="delete">
+                    </div>
+                </div>
+                */
 
 // click hover the element
-boxElem.addEventListener("click", function () {
-    if (task.value !== "") {
-
-        let newElement = document.createElement('div'); // is a node
-        newElement.classList.add('inLine');
-        newElement.innerHTML = `
+btnAddTask.addEventListener("click", function () {
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('inLine');
+    let newElement = document.createElement('div'); // is a node
+    newElement.classList.add('inLine');
+    newElement.innerHTML = `
             <p class="todone">${task.value}</p>
             <div class="buttons">
                 <button class="done">Done</button>
                 <button class="delete">Delete</button>
             </div>
         `;
-        row.appendChild(newElement); // add the content of the inLineRow div
-        task.value = ""; // to clear the input task 
-        let buttons = document.querySelectorAll(".inLine .buttons button");
-        console.log(buttons);
+    if (task.value === "") {
+        return null;
+    }
+    row.appendChild(newElement); // add the content of the inLineRow div
+    task.value = ""; // to clear the input task 
+    let buttons = document.querySelectorAll(".inLine .buttons button");
+    console.log(buttons);
 
-        buttons.forEach(btn => {
-            btn.addEventListener('click', function () {
-                if (btn.classList[0] === 'done') {
-                    btn.classList.toggle('undo'); // done <=> undo
-                    console.log(btn);
-                    // new value of Done button
-                    if (btn.innerHTML === "Done") {
-                        btn.innerHTML = "Undo";
-                    } else {
-                        btn.innerHTML = "Done";
-                    }
-                };
-                if (btn.classList.contains('delete')) {
-                    btn.parentElement.parentElement.remove() // the granddad from the element and remove it
+    buttons.forEach(btn => {
+        btn.addEventListener('click', function () {
+            if (btn.classList[0] === 'done') {
+                btn.classList.toggle('undo'); // done <=> undo
+                console.log(btn);
+                // new value of Done button
+                if (btn.innerHTML === "Done") {
+                    btn.innerHTML = "Undo";
+                } else {
+                    btn.innerHTML = "Done";
                 }
-            });
+            };
+            if (btn.classList.contains('delete')) {
+                btn.parentElement.parentElement.remove() // the granddad from the element and remove it
+            }
         });
-    }
-    else {
-        alert('input the value')
-    }
+    });
+}
+   
 });
 
